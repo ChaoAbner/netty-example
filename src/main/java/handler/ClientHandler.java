79a616fe0c9e4f926c65608d581ba466aaac4724
c201911101407344 <YC@FOSUCHAO.COM>
2019-11-25 20:16:39 +0800

    add handler

--- src/main/java/handler/ClientHandler.java
+++ src/main/java/handler/ClientHandler.java
@@ -1,9 +1,22 @@
 package handler;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
 /**
  * @Description:
  * @Auther: Joker Ye
  * @Date: 2019/11/24 23:53
  */
-public class ClientHandler {
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("client接受到服务器返回的信息" + msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
 }
--- src/main/java/handler/ServerHandler.java
+++ src/main/java/handler/ServerHandler.java
@@ -1,9 +1,34 @@
 package handler;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
 /**
- * @Description:
+ * @Description: https://blog.csdn.net/qq_35357001/article/details/77715451
  * @Auther: Joker Ye
  * @Date: 2019/11/24 23:42
  */
-public class ServerHandler {
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 从管道读取数据
+        System.out.println("server received data: " + msg);
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // 读取数据完成
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)    // flush调所有写回的数据
+                .addListener(ChannelFutureListener.CLOSE);  // 当flash完成后关闭Channel
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();    // 捕获异常信息
+        ctx.close();            // 出现异常则关闭channel
+    }
 }
